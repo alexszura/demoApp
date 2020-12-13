abfd5686d66ace09ef9102e518a20404f411b356
Alexander Szura <szuraale@msu.edu>
2020-12-13 15:21:49 -0500

    Fixed errors/submission

--- tipcalculator/ViewController.swift
+++ tipcalculator/ViewController.swift
@@ -14,6 +14,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var billfield: UITextField!
+    
+    @IBOutlet weak var tipcontrol: UISegmentedControl!
+    @IBOutlet weak var totallabel: UILabel!
+    @IBOutlet weak var tiplabel: UILabel!
+    @IBAction func calculatetip(_ sender: Any) {
+        // Get bill amount
+        let bill = Double(billfield.text!) ?? 0
+            
+        // Calculate tip and total
+        let tippercentages = [0.15,0.18,0.20]
+        
+        let tip = bill * tippercentages[tipcontrol.selectedSegmentIndex]
+        let total = bill + tip
+        // Update tip and total
+        tiplabel.text = String(format: "$%.2f", tip)
+        totallabel.text = String(format: "$%.2f", total)
+        
+    }
+    @IBAction func ontap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
 }
 
