2ef4394f2352455b347b0a9407e7d422e2ddcde3
Alexander Szura <szuraale@msu.edu>
2020-12-12 22:37:46 -0500

    Initial Commit

---
+++ tipcalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tipcalculator
+//
+//  Created by Sasha Szura on 12/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
